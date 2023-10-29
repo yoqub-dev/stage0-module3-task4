8a0c0e24d2d596f3973570925bc6ca641d9fa2fe
yoqub-dev <yoqub.epam@gmail.com>
2023-10-29 21:27:07 +0500

    task4

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.module3.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        int a = 5;
+        int b = 5;
+
+        int a1 = a * 2 * 9;
+        int b1 = b * 5;
+        int result = a1 - b1 + 2 + a - 7;
+        int result1 = (a + b - 4 * a * b) / 2;
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        System.out.println((int) c);
+        System.out.println((int) h);
+        System.out.println((int) a);
+        System.out.println((int) r);
     }
 }
